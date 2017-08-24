4aa537069c18c07dbd1473ff731c5621dc0b6734
Tom Weatherhead <tom_weatherhead@hotmail.com>
2017-08-24 11:29:59 -0600

    Ready to release version 0.0.0

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,93 +1,93 @@
-"use strict";
+'use strict';
 
-var fs = require("fs"),
-	path = require("path");
+const fs = require('fs'),
+	path = require('path');
 
 module.exports = function(grunt) {
 	grunt.initConfig({
-		pkg : grunt.file.readJSON("package.json"),
+		pkg : grunt.file.readJSON('package.json'),
 		concat: {
 			options : {
-				banner : "/**\n" + 
-				         " * <%= pkg.name %>\n" +
-				         " *\n" +
-				         " * @copyright <%= grunt.template.today('yyyy') %> <%= pkg.author.name %>\n" +
-				         " * @license <%= pkg.license %>\n" +
-				         " * @version <%= pkg.version %>\n" +
-				         " */\n"
+				banner : '/**\n' + 
+				         ' * <%= pkg.name %>\n' +
+				         ' *\n' +
+				         ' * @copyright <%= grunt.template.today(\'yyyy\') %> <%= pkg.author.name %>\n' +
+				         ' * @license <%= pkg.license %>\n' +
+				         ' * @version <%= pkg.version %>\n' +
+				         ' */\n'
 			},
 			dist: {
 				src : [
-					"<banner>",
-					"src/intro.js",
-					"src/gcd.js",
-					"src/outro.js"
+					'<banner>',
+					'src/intro.js',
+					'src/<%= pkg.name %>.js',
+					'src/outro.js'
 				],
-				dest : "lib/thaw-gcd.es6.js"
+				dest : 'lib/<%= pkg.name %>.es6.js'
 			}
 		},
-		"babel": {
+		'babel': {
 			options: {
 				sourceMap: false,
-				presets: ["babel-preset-es2015"]
+				presets: ['babel-preset-es2015']
 			},
 			dist: {
 				files: {
-					"lib/<%= pkg.name %>.js": "lib/<%= pkg.name %>.es6.js"
+					'lib/<%= pkg.name %>.js': 'lib/<%= pkg.name %>.es6.js'
 				}
 			}
 		},
 		eslint: {
-			target: ["lib/<%= pkg.name %>.es6.js"]
+			target: ['lib/<%= pkg.name %>.es6.js']
 		},
 		nodeunit : {
-			all : ["test/*.js"]
+			all : ['test/*.js']
 		},
 		uglify: {
 			options: {
-				banner: "/*\n<%= grunt.template.today('yyyy') %> <%= pkg.author.name %>\n @version <%= pkg.version %>\n*/",
+				banner: '/*\n<%= grunt.template.today(\'yyyy\') %> <%= pkg.author.name %>\n @version <%= pkg.version %>\n*/',
 				sourceMap: true,
 				sourceMapIncludeSources: true
 			},
 			target: {
 				files: {
-					"lib/thaw-gcd.min.js" : ["lib/thaw-gcd.js"]
+					'lib/<%= pkg.name %>.min.js' : ['lib/<%= pkg.name %>.js']
 				}
 			}
 		},
 		watch : {
 			js : {
-				files : "<%= concat.dist.src %>",
-				tasks : "build"
+				files : '<%= concat.dist.src %>',
+				tasks : 'build'
 			},
 			pkg: {
-				files : "package.json",
-				tasks : "build"
+				files : 'package.json',
+				tasks : 'build'
 			}
 		}
 	});
 
-	// tasks
-	grunt.loadNpmTasks("grunt-contrib-concat");
-	grunt.loadNpmTasks("grunt-contrib-nodeunit");
+	// Tasks
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-nodeunit');
 	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks("grunt-contrib-uglify");
-	grunt.loadNpmTasks("grunt-babel");
-	grunt.loadNpmTasks("grunt-eslint");
-	grunt.task.registerTask("babili", "Minifies ES2016+ code", function () {
-		var data = fs.readFileSync(path.join(__dirname, "lib", "thaw-gcd.es6.js"), "utf8"),
-			minified = require("babel-core").transform(data, {sourceFileName: "thaw-gcd.es6.js", sourceMaps: true, presets: ["babili"]}),
-			pkg = require(path.join(__dirname, "package.json")),
-			banner = "/*\n " + new Date().getFullYear() + " " + pkg.author.name + "\n @version " + pkg.version + "\n*/\n\"use strict\";";
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-babel');
+	grunt.loadNpmTasks('grunt-eslint');
+	grunt.task.registerTask('babili', 'Minifies ES2016+ code', function () {
+		var data = fs.readFileSync(path.join(__dirname, 'lib', 'thaw-gcd.es6.js'), "utf8"),
+			minified = require('babel-core').transform(data, {sourceFileName: 'thaw-gcd.es6.js', sourceMaps: true, presets: ['babili']}),
+			pkg = require(path.join(__dirname, 'package.json')),
+			banner = '/*\n ' + new Date().getFullYear() + ' ' + pkg.author.name + '\n @version ' + pkg.version + '\n*/\n\'use strict\';';
 
-		fs.writeFileSync(path.join(__dirname, "lib", "thaw-gcd.es6.min.js"), banner + minified.code + "\n//# sourceMappingURL=thaw-gcd.es6.min.js.map", "utf8");
-		grunt.log.ok("1 file created.");
-		fs.writeFileSync(path.join(__dirname, "lib", "thaw-gcd.es6.min.js.map"), JSON.stringify(minified.map), "utf8");
-		grunt.log.ok("1 sourcemap created.");
+		fs.writeFileSync(path.join(__dirname, 'lib', 'thaw-gcd.es6.min.js'), banner + minified.code + '\n//# sourceMappingURL=thaw-gcd.es6.min.js.map', 'utf8');
+		grunt.log.ok('1 file created.');
+		fs.writeFileSync(path.join(__dirname, 'lib', 'thaw-gcd.es6.min.js.map'), JSON.stringify(minified.map), 'utf8');
+		grunt.log.ok('1 sourcemap created.');
 	});
 
 	// aliases
-	grunt.registerTask("test", ["eslint", "nodeunit"]);
-	grunt.registerTask("build", ["concat", "babel"]);
-	grunt.registerTask("default", ["build", "test", "babili", "uglify"]);
+	grunt.registerTask('test', ['eslint', 'nodeunit']);
+	grunt.registerTask('build', ['concat', 'babel']);
+	grunt.registerTask('default', ['build', 'test', 'babili', 'uglify']);
 };
--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # thaw-gcd
-Euclid's greatest common divisor algorithm as a JavaScript component.
+Euclid's greatest common divisor algorithm as a JavaScript component and npm package.
+
+See https://en.wikipedia.org/wiki/Euclidean_algorithm .
+
+Git installation instructions:
+
+	$ git clone https://github.com/tom-weatherhead/thaw-gcd.git
+	$ cd thaw-gcd
+	$ npm install -g grunt
+	$ npm install
+	$ grunt
+
+npm Installation Instructions:
+
+	$ npm install [--save] thaw-gcd
+
+Sample usage of the npm package in a Node.js application:
+
+	$ git init thaw-gcd-client-app
+	$ cd thaw-gcd-client-app
+	$ npm init -y
+	$ npm install --save thaw-gcd
+	- Save the following three lines of code in the file index.js :
+	
+		const gcd = require('thaw-gcd');
+
+		console.log('gcd is', gcd);
+		console.log('gcd(35, 84) ===', gcd(35, 84));	// The result should be 7
+
+	$ node index.js
+
+For examples of client-side use of this package, see the "examples" directory.
